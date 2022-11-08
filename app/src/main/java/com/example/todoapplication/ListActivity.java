50380042a6a28dfc5d163f33997409272c8784bc
kajikawa256 <kajikawa2003@gmail.com>
2022-11-08 12:21:15 +0900

    タイトル、内容の必須入力 リスト画面でのタイトル表示完了

--- app/src/main/java/com/example/todoapplication/ListActivity.java
+++ app/src/main/java/com/example/todoapplication/ListActivity.java
@@ -7,14 +7,18 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ListActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +32,22 @@ public class ListActivity extends AppCompatActivity {
         // プレファレンスの宣言
         SharedPreferences pref;
         ArrayList<String> titleList;
-        ArrayList<String> memoList;
 
         // リストを読み込む
+        pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        Set<String> sArray = pref.getStringSet("ToDoTitle",null);
 
-        // 登録ボタンを押したときの処理
+        if(sArray!=null){
+            titleList = new ArrayList<>(sArray);
+            ArrayAdapter<String> arrayAdapter =
+                    new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titleList);
+            listView.setAdapter(arrayAdapter);
+        }else{
+            // プリファレンスが空の場合
+            titleList = new ArrayList<>();
+        }
+
+        // Todo登録ボタンを押したときの処理
         regibutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -40,6 +55,5 @@ public class ListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
 }
--- app/src/main/java/com/example/todoapplication/RegisterActivity.java
+++ app/src/main/java/com/example/todoapplication/RegisterActivity.java
@@ -3,12 +3,17 @@ package com.example.todoapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -24,10 +29,22 @@ public class RegisterActivity extends AppCompatActivity {
         Button backButton = findViewById(R.id.backButton);
         Button regiButton = findViewById(R.id.registerButton);
 
-        // リスト取得
-        Intent getintent = getIntent();
-        ArrayList<String> titleList = getintent.getStringArrayListExtra("titleList");
-        ArrayList<String> contentList = getintent.getStringArrayListExtra("titleList");
+        // プレファレンスの宣言
+        SharedPreferences pref;
+        ArrayList<String> titleList;
+        ArrayList<String> memoList;
+
+        // プリファレンスから値を取得し、リストを初期化する
+        pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        Set<String> sArray = pref.getStringSet("ToDoTitle",null);
+        Set<String> s2Array = pref.getStringSet("ToDoMemo",null);
+        if(sArray != null && s2Array != null){
+            titleList = new ArrayList<>(sArray);
+            memoList = new ArrayList<>(s2Array);
+        }else{
+            titleList = new ArrayList<>();
+            memoList = new ArrayList<>();
+        }
 
         // 戻るボタンクリック時の処理
         backButton.setOnClickListener(new View.OnClickListener(){
@@ -42,14 +59,24 @@ public class RegisterActivity extends AppCompatActivity {
         regiButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                String title = titlefont.getText().toString();
-                String content = contentfont.getText().toString();
-                titleList.add(title);
-                contentList.add(content);
-                Intent intent = new Intent(RegisterActivity.this,ListActivity.class);
-                intent.putStringArrayListExtra("titleList",titleList);
-                intent.putStringArrayListExtra("contentList",contentList);
-                startActivity(intent);
+                if(titlefont.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),"タイトルを入力してください",Toast.LENGTH_SHORT).show();
+                }else if(contentfont.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),"内容を入力してください",Toast.LENGTH_SHORT).show();
+
+                }else{
+                    // タイトルと内容取得、追加
+                    titleList.add(titlefont.getText().toString());
+                    memoList.add(contentfont.getText().toString());
+                    // キャストしてプリファレンスへ登録
+                    HashSet<String> cArray = new HashSet<String>(titleList);
+                    HashSet<String> dArray = new HashSet<String>(memoList);
+                    pref.edit().putStringSet("ToDoTitle",cArray).apply();
+                    pref.edit().putStringSet("ToDoMemo",dArray).apply();
+                    // リストアクティビティを起動
+                    Intent intent = new Intent(RegisterActivity.this,ListActivity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
